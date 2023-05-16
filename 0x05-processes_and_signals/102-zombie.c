9314c0f71460d3cf9725462836145167993d0e86
root <sisayfikru69@gmail.com>
2023-05-16 00:17:14 -0700

    102-zombie.c

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -8,11 +8,11 @@
  */
 int infinite_while(void)
 {
-  while (1)
-    {
-      sleep(1);
-    }
-  return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 
 /**
@@ -25,13 +25,13 @@ int main(void)
 	pid_t zombie;
 
 	for (i = 0; i < 5; i++)
-    {
-      zombie = fork();
-      if (!zombie)
-	      return (0);      
-      printf("Zombie process created, PID: %d\n", zombie);
-    }
-  
-  infinite_while();
-  return (0);
+	{
+		zombie = fork();
+		if (!zombie)
+			return (0);
+		printf("Zombie process created, PID: %d\n", zombie);
+	}
+
+	infinite_while();
+	return (0);
 }
