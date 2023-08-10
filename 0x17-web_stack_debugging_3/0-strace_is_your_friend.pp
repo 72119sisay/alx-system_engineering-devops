a1a44bc0a0dac3677f3c924a0701cdd10713b863
72119sisay <sisayfikru69@gmail.com>
2023-08-10 11:26:37 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Puppet manifest to fix a bug in wp-setings.php
 
 exec { 'fix the php extension issue':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  command => 'sed -i 's/phpp/php/g' /var/www/html/index.html',
   path    => '/usr/local/bin/:/bin/'
 }
